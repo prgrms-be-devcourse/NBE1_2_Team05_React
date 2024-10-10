8e2a206581b6b798d8f78805badd8eecf87bea8c
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-10-10 10:40:07 +0900

    feat: 내 정보 디자인 수정

--- src/page/member/MemberProfilePage.js
+++ src/page/member/MemberProfilePage.js
@@ -13,7 +13,9 @@ const MemberProfilePage = () => {
                 <Container fixed>
                     <div>
                         <h1>마이페이지</h1>
+                        <h2>내 정보</h2>
                         <MemberInfo />
+                        <hr/>
                         <h2>내가 등록한 공연</h2>
                         <MyPerformances/>
                         <hr/>
